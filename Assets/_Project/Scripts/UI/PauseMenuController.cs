7e5425cc69162f1c51c6cc467bdfcb954439db58
youugotssponged <jordanmccann64@outlook.com>
2022-01-29 13:53:27 +0000

    Pause Menu Part 1

---
+++ Assets/_Project/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PauseMenuController : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioClip BtnClick;
+    public AudioClip BtnHover;
+
+    public bool isPaused;
+    public GameObject PausePanel;
+    public GameObject SettingsPanel;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        isPaused = false;
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            _ = isPaused ? Resume() : Pause(); // Cheeky Discard   
+    }
+
+
+
+    // Apparently Trigger event handlers need to be VOID and no type... ffs
+    public void PauseUI() => Pause();
+    public void ResumeUI() => Resume();
+
+
+    // UI Event handlers
+    public bool Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+
+        return true;
+    }
+    public bool Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        PausePanel.SetActive(false);
+
+        // Close settings if not already closed, this deals with the issue of having settings open and unpausing
+        CloseSettingsPanel();
+
+        return isPaused;
+    }
+    public void OpenSettings() 
+    {
+        if(isPaused)
+        {
+            PausePanel.SetActive(false);
+            SettingsPanel.SetActive(true);
+        }
+    }
+    public void CloseSettings() 
+    { 
+        PausePanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+    }
+
+    public void CloseSettingsPanel() => SettingsPanel.SetActive(false);
+
+    public void BackToMainMenu() 
+    {
+        Time.timeScale = 1f; // resume engine time before moving back
+        GlobalSceneManager._Instance.UpdateSceneManagerState(GlobalSceneManager.SceneManagerState.MAINMENU);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+    public void PlaySoundOnClick() => AudioSource.PlayOneShot(BtnClick);
+    public void PlaySoundOnHover() => AudioSource.PlayOneShot(BtnHover);
+
+
+}
